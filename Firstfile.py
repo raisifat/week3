2bd98a8e935ed06d96e54f81fbec1fa2a12183b1
raisifat <rayansifat2007@gmail.com>
2024-09-24 10:04:10 -0400

    firstfile.py

--- Firstfile.py
+++ Firstfile.py
@@ -1,6 +1,9 @@
-
 #rayan
 #9/23/2024
- 
-print(Rayan, Sifat, age= 17) 
-print(Rayan,Sifat)
+
+first = "Rayan" 
+last = "Sifat" 
+age = 17
+
+print(first + " "+ last +" and I'm 17 years old") 
+print(first,last,"and I'm", age, "years old")
